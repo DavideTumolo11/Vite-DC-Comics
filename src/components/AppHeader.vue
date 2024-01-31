01bc1c73fb8f7009e9250637a125071e1f2bb5b8
DavideTumolo11 <davidetumolo11@gmail.com>
2024-01-31 22:09:39 +0100

    second_commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+
+<template>
+    <header>
+
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+
+        <nav>
+            <ul class="main_menu">
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+header {
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+
+    & .main_menu {
+        list-style: none;
+        display: flex;
+        gap: 50px;
+    }
+
+    & .logo {
+        padding-top: 20px;
+        padding-bottom: 20px;
+    }
+
+    & a {
+        text-decoration: none;
+        color: black;
+        font-size: 12px;
+
+    }
+
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    name: 'AppMain'
+}
+</script>
+
+
+<template>
+    <main>
+        <div class="components_space"> --Content goes here--</div>
+        <div class="buy_space">
+            <img class="img_buy" src="../assets/img/buy-comics-digital-comics.png" alt="">DIGITAL COMICS
+            <img class="img_buy" src="../assets/img/buy-comics-merchandise.png" alt="">DC MERCHANDISE
+            <img class="img_buy" src="../assets/img/buy-comics-subscriptions.png" alt="">SUBSCRIPTION
+            <img class="img_buy" src="../assets/img/buy-comics-shop-locator.png" alt="">COMIC SHOP LOCATOR
+            <img class="img_buy" src="../assets/img/buy-dc-power-visa.svg" alt="">DC POWER VISA
+        </div>
+    </main>
+</template>
+
+<style scoped>
+.components_space {
+    background-color: var(--soft-black);
+    height: 200px;
+    color: white;
+    display: flex;
+    align-items: center;
+    font-size: 30px;
+    padding-left: 200px;
+}
+
+.buy_space {
+    background-color: var(--soft-blue);
+    height: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    justify-content: space-evenly;
+    color: white;
+}
+
+.img_buy {
+    height: 60px;
+
+}
+</style>
