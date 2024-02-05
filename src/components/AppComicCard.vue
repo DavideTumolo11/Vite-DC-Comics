ed8274a5d55c26234de16b25de4c54da281ac5f9
DavideTumolo11 <davidetumolo11@gmail.com>
2024-02-05 21:29:18 +0100

    comiccard

---
+++ src/components/AppComicCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: 'AppComicCard',
+    props: {
+        thumb: String,
+        title: String
+    }
+
+}
+</script>
+
+
+<template>
+    <div class="card">
+        <img :src="thumb" :alt="title + 'image'" alt="">
+        <h5>{{ title }}</h5>
+    </div>
+</template>
+
+
+<style scoped>
+.card {
+
+    &>img {
+        aspect-ratio: 1;
+        object-fit: cover;
+        object-position: top;
+    }
+}
+</style>
